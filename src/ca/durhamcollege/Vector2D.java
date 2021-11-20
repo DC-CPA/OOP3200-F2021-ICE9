835190dd36880665f48ffc60e91c369af8edb7df
Katherine Bellman <katherine.bellman@dcmail.ca>
2021-11-19 20:11:00 -0500

    added operator methods for calculations of vectors

--- src/ca/durhamcollege/Vector2D.java
+++ src/ca/durhamcollege/Vector2D.java
@@ -27,7 +27,7 @@ public class Vector2D {
         this.y = y;
     }
 
-    public void Set(float x, float y)
+    public void set(float x, float y)
     {
         this.x =x;
         this.y = y;
@@ -38,6 +38,7 @@ public class Vector2D {
     {
         return x;
     }
+
     public float getY()
     {
         return y;
@@ -52,13 +53,13 @@ public class Vector2D {
 
     Vector2D(float x, float y)
     {
-        Set(x,y);
+        set(x,y);
     }
 
                 //=====Copy constructor=====
     Vector2D(@NotNull Vector2D vector)
     {
-        Set(vector.getX(), vector.getY());
+        set(vector.getX(), vector.getY());
     }
 
     //==========PRIVATE METHODS==========
@@ -101,17 +102,27 @@ public class Vector2D {
         return (float) Math.sqrt(this.getX() * this.getX() + this.getY() * this.getY());
     }
 
+    public float getSqrMagnitude()
+    {
+        return (this.getX() * this.getX() + this.getY() * this.getY());
+    }
+
     public void setScale(float scale)
     {
         this.set(this.getX() * scale, this.getY() * scale);
     }
 
-    public void normaize()
+    public void setScale(Vector2D scale)
+    {
+        this.set(this.getX() * scale.x, this.getY() * scale.y);
+    }
+
+    public void normalize()
     {
         var magnitude = this.getMagnitude();
         if ((double)(magnitude) > 9.9999974737875E-06)
         {
-            Set(GetX() / magnitude, GetY()/ magnitude);
+            set(getX() / magnitude, getY()/ magnitude);
         }
         else
         {
@@ -122,7 +133,7 @@ public class Vector2D {
     public Vector2D getNormalized()
     {
         Vector2D vector = new Vector2D(getX(), getY());
-        vector.Normalize();
+        vector.normalize();
         return vector;
     }
 
@@ -134,5 +145,61 @@ public class Vector2D {
 
     //==========STATIC METHODS==========
 
+    public static Vector2D zero()
+    {
+        return new Vector2D(0.0f, 0.0f);
+    }
+
+    public static Vector2D one()
+    {
+        return new Vector2D(1.0f, 1.0f);
+    }
+
+    public static Vector2D left()
+    {
+        return new Vector2D(-1.0f, 0.0f);
+    }
+
+    public static Vector2D right()
+    {
+        return new Vector2D(0.0f, -1.0f);
+    }
+
+    public static Vector2D up()
+    {
+        return new Vector2D(0.0f, 1.0f);
+    }
+
+    public static Vector2D down()
+    {
+        return new Vector2D(0.0f, -1.0f);
+    }
+
+    public static Vector2D lerp(final Vector2D a, final Vector2D b, float t)
+    {
+        if ((double)(t) < 0.0)
+        {
+            t = 0.0f;
+        }
+        if ((double)(t) > 1.0)
+        {
+            t = 1.0f;
+        }
+
+        return new Vector2D(a.getX() + (b.getX() - a.getX()) * t, a.getY() + (b.getY() - a.getY()) * t);
+    }
+
+    public static  float dot(final Vector2D lhs, final Vector2D rhs)
+    {
+        return (float)((double)(lhs.getX()) * (double)(rhs.getX()) + (double)(lhs.getY()) * (double)(rhs.getY()));
+    }
+
+    public static float distance(final Vector2D a, final Vector2D b)
+    {
+        final  var delta_x = (double)(b.getX()) - (double)(a.getX());
+        final  var delta_y = (double)(b.getY()) - (double)(a.getY());
+
+        return ((float)(delta_x * delta_x + delta_y * delta_y));
+    }
 
 }
